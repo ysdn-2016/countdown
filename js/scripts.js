fb1ac85fe52515007fb964d3e891d16b0277327e
Nathan Butler <iamnbutler@gmail.com>
2015-09-17 01:57:01 -0400

    Fix3

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 // Randomize images
 
 function randomize_bg(){
-	var myimages=["countdown/img/1_nichijou.gif", "countdown/img/3_10-kanyes-greatest-quotes-545.gif", "countdown/img/4_200.gif", "countdown/img/5_giphy-1.gif", "countdown/img/6_giphy-2.gif", "countdown/img/7_giphy-4.gif", "countdown/img/8_giphy-6.gif", "countdown/img/9_giphy-7.gif", "countdown/img/10_giphy-8.gif", "countdown/img/11_giphy.gif", "countdown/img/12_Life-sucks-adventure-time-with-finn-and-jake-34268644-400-290.gif", "countdown/img/13_Personas-que-debes-evitar.gif"];
+	var myimages=["img/1_nichijou.gif", "img/3_10-kanyes-greatest-quotes-545.gif", "img/4_200.gif", "img/5_giphy-1.gif", "img/6_giphy-2.gif", "img/7_giphy-4.gif", "img/8_giphy-6.gif", "img/9_giphy-7.gif", "img/10_giphy-8.gif", "img/11_giphy.gif", "img/12_Life-sucks-adventure-time-with-finn-and-jake-34268644-400-290.gif", "img/13_Personas-que-debes-evitar.gif"];
 
 	var ry=Math.floor(Math.random()*myimages.length);
 	if (ry===0) {
